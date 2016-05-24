ef9e3f2155ebfef4fa62e1e5772bab828c0e964c
Benjamin Pucher <bp@alladin.at>
2016-05-24 18:01:30 +0200

    start with rmbt-ios-client framework project

---
+++ Sources/RMBTClient.h
@@ -0,0 +1,19 @@
+//
+//  RMBTClient.h
+//  RMBTClient
+//
+//  Created by Benjamin Pucher on 24.05.16.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RMBTClient.
+FOUNDATION_EXPORT double RMBTClientVersionNumber;
+
+//! Project version string for RMBTClient.
+FOUNDATION_EXPORT const unsigned char RMBTClientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RMBTClient/PublicHeader.h>
+
+
---
+++ Sources/Test.swift
@@ -0,0 +1,9 @@
+//
+//  Test.swift
+//  rmbt-ios-client
+//
+//  Created by Benjamin Pucher on 24.05.16.
+//
+//
+
+import Foundation
