4c12a8b967ab82bac274ebf8d7ee4d9ea19a1ea3
Benjamin Pucher <bp@alladin.at>
2016-09-28 15:28:49 +0200

    add apache2 license header

--- Sources/PingUtilSwiftDelegate.swift
+++ Sources/PingUtilSwiftDelegate.swift
@@ -1,10 +1,18 @@
-//
-//  PingUtilSwiftDelegate.swift
-//  RMBT
-//
-//  Created by Benjamin Pucher on 09.02.15.
-//  Copyright © 2015 SPECURE GmbH. All rights reserved.
-//
+/*****************************************************************************************************
+ * Copyright 2014-2016 SPECURE GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************************************/
 
 import Foundation
 
--- Sources/RMBTClient.h
+++ Sources/RMBTClient.h
@@ -1,10 +1,18 @@
-//
-//  RMBTClient.h
-//  RMBTClient
-//
-//  Created by Benjamin Pucher on 24.05.16.
-//
-//
+/*****************************************************************************************************
+ * Copyright 2016 SPECURE GmbH
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *****************************************************************************************************/
 
 @import Foundation;
 
@@ -15,17 +23,6 @@ FOUNDATION_EXPORT double RMBTClientVersionNumber;
 FOUNDATION_EXPORT const unsigned char RMBTClientVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <RMBTClient/PublicHeader.h>
-
-// background traffic, ram, cpu usage
-/*#import <RMBTClient/RMBTTrafficCounter.h>
-#import <RMBTClient/RMBTRAMMonitor.h>
-#import <RMBTClient/RMBTCPUMonitor.h>
-
-#import <RMBTClient/GetDNSIP.h>
-
-#import <RMBTClient/NSString+IPAddress.h>
-#import <RMBTClient/PingUtil.h>*/
-
 
 #import "RMBTTrafficCounter.h"
 #import "RMBTRAMMonitor.h"
